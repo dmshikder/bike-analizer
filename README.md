b40042cc988a24826484bbf1ebac0a5fad40c3d4
dmshikder <dmshikder@gmail.com>
2022-05-30 18:37:53 +0600

    read me file created

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 live link: [Bike-analizer ](https://candid-caramel-ca3eec.netlify.app/)
 
+About this project:
+
+- I have used react router for creating SPA
+- Used rechart for dashboard
+- used custom hook for multiple use
+- Written blog
+
 # product-analysis-website-dmshikder
--- src/components/HomeReview/HomeReview.js
+++ src/components/HomeReview/HomeReview.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useReview from '../../hooks/useReview/useReview';
 import Review from '../Review/Review';
+import './HomeReview.css';
 
 const HomeReview = (props) => {
     const [reviews, setReviews] = useReview();
-    console.log(reviews)
+       
+    // const newReview = reviews.slice(0,3)
+  
     return (
         <div>
             <h3 className='text-3xl font-bold p-10  '>Review from our beloved customer</h3>
          <Review reviews={reviews}></Review>
-         <button  className='bg-red-400 p-3  rounded m-10'>See All Reviews</button>
+         <button  className='btn p-3 px-10 rounded m-10'><Link to="/review">See All Review</Link></button>
 
             </div>
         
